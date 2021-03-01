658b5ab50a44bf34fb8a6406869251f3a1c4b51a
Vuong-Chu <42327886+Vuong-Chu@users.noreply.github.com>
2021-03-01 16:12:02 +0000

    Add files via upload

---
+++ AreaCalculator.java
@@ -0,0 +1,28 @@
+/**
+ * This exercise is to practice how to write method inside class
+ * @author VuongChu
+ *
+ */
+public class AreaCalculator{
+    public static void main(String[] args) {
+        System.out.println(area(5));
+        System.out.println(area(5,4));
+    }
+     
+    public static double area(double radius){
+        if(radius<0) {
+            return -1.0;
+        }else {
+            return Math.pow(radius, 2)*Math.PI;
+        }
+    }
+    
+    public static double area(double x, double y) {
+        if(x<0 || y<0) {
+            return -1.0;
+        }else {
+            return x*y;
+        }
+    }
+    
+}
