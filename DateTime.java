14f95d3ef88c4bcbb369a856096b97140eb37591
Vuong Chu <42327886+Vuong-Chu@users.noreply.github.com>
2021-07-08 20:38:29 +0100

    Update DateTime.java

--- DateTime.java
+++ DateTime.java
@@ -1,4 +1,6 @@
 import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class DateTime{
     public static void main(String[] args)  {
@@ -6,7 +8,31 @@ public class DateTime{
         System.out.println(LocalTime.now());
         System.out.println(LocalDateTime.now());
         System.out.println(ZonedDateTime.now());
-        System.out.println(LocalDate.of(2021, Month.JULY, 6));
-        System.out.println(LocalTime.of(7,20));
+        LocalDate date = LocalDate.of(2021, Month.JULY, 6);
+        LocalTime time = LocalTime.of(7,20);
+        System.out.println(date);
+        System.out.println(time);
+
+        ZoneId zone = ZoneId.of("US/Eastern");
+        ZonedDateTime zonedDateTime= ZonedDateTime.of(date, time, zone);
+        System.out.println(zonedDateTime);
+        System.out.println(zonedDateTime.plusDays(1));
+        System.out.println(zonedDateTime.plusWeeks(1));
+        System.out.println(zonedDateTime.minusMonths(1));
+        System.out.println(ZoneId.systemDefault());
+
+        //Period for date
+        Period annualy = Period.ofYears(1);
+        Period everyYearAndAWeek = Period.of(1,0,7);
+
+        //Duration for time
+        Duration daily = Duration.ofDays(1);
+        Duration daily_v2 = Duration.of(1, ChronoUnit.DAYS);
+        Duration nano = Duration.ofNanos(1);
+        Duration nano_v2 = Duration.of(1,ChronoUnit.NANOS);
+
+        System.out.println(Locale.GERMAN);
+        System.out.println(Locale.GERMANY);
+
     }
 }
