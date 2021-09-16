7f0bc5145de29c39ef98fbc52e40328879bb5216
Vuong Chu <42327886+Vuong-Chu@users.noreply.github.com>
2021-09-16 09:10:45 +0100

    Add files via upload

---
+++ Concurrency4.java
@@ -0,0 +1,44 @@
+public class Concurrency4 {
+    public static void main(String[] args){
+        Counter ct = new Counter();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ct.getCounter();
+            }
+        },"thread 1");
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ct.continueCounter();
+            }
+        }, "thead 2");
+        t2.start();
+        t1.start();
+    }
+}
+
+class Counter{
+    volatile boolean done = false;
+    int sum;
+    public synchronized void getCounter(){
+        System.out.println(Thread.currentThread().getName() +" is starting.");
+        for(int i=0; i<100000; i++){
+            sum++;
+        }
+        done = true;
+        notify();
+    }
+
+    public synchronized void continueCounter(){
+        while (!done) {
+            try {
+                System.out.println(Thread.currentThread().getName() +" is waiting.");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(sum+1000);
+    }
+}
